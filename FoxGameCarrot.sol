48c891502301a014466bfbf1b0e9cef159b83135
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-12-02 16:30:45 +0530

    Update FoxGameCarrot.sol

--- FoxGameCarrot.sol
+++ FoxGameCarrot.sol
@@ -11,8 +11,8 @@
 
 pragma solidity ^0.8.10;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Ownable.sol";
+import "./ERC20.sol";
 
 import "./IFoxGameCarrot.sol";
 
